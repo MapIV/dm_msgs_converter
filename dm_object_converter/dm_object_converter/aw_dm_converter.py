8f13e53f96a4d81b8a036bffeca666c4338824b0
amc-nu <abrahammonrroy@yahoo.com>
2022-02-22 19:49:54 +0900

    wip. aw to dm conversion
    Signed-off-by: amc-nu <abrahammonrroy@yahoo.com>

--- dm_object_converter/dm_object_converter/aw_dm_converter.py
+++ dm_object_converter/dm_object_converter/aw_dm_converter.py
@@ -6,8 +6,12 @@ from autoware_perception_msgs.msg import DynamicObjectWithFeature
 from autoware_perception_msgs.msg import DynamicObjectWithFeatureArray
 from unique_identifier_msgs.msg import UUID
 from autoware_perception_msgs.msg import DynamicObject
+from autoware_perception_msgs.msg import Semantic
 from autoware_perception_msgs.msg import DynamicObjectArray
 from dm_object_info_msgs.msg import ObjectInfo
+from dm_object_info_msgs.msg import ObjectInfoArray
+from dm_object_info_msgs.msg import ObjectClass
+from dm_object_info_msgs.msg import ClassId
 
 
 class AwDmConverter(Node):
@@ -22,12 +26,33 @@ class AwDmConverter(Node):
             self.aw_perception_callback,
             10)
 
-    def aw_perception_callback(self, aw_msg):
-        if len(aw_msg.feature_objects) > 0:
-            self.get_logger().info("Message Received")
-            msg = String()
-            msg.data = f"{aw_msg.feature_objects[0].object.id}"
-            self.publisher_.publish(msg)
+    def aw_class_to_dm(self, dynamic_object: DynamicObject):
+        dm_object_class = ObjectClass()
+        if dynamic_object.semantic.type == Semantic.CAR \
+                or dynamic_object.semantic.type == Semantic.TRUCK \
+                or dynamic_object.semantic.type == Semantic.BUS \
+                or dynamic_object.semantic.type == Semantic.BICYCLE \
+                or dynamic_object.semantic.type == Semantic.MOTORBIKE:
+            dm_object_class.id.value = ClassId.VEHICLE
+        if dynamic_object.semantic.type == Semantic.PEDESTRIAN:
+            dm_object_class.id.value = ClassId.PERSON
+        if dynamic_object.semantic.type == Semantic.ANIMAL:
+            dm_object_class.id.value = ClassId.ANIMAL
+        if dynamic_object.semantic.type == Semantic.UNKNOWN:
+            dm_object_class.id.value = ClassId.OTHER
+        dm_object_class.confidence.value = dynamic_object.semantic.confidence*100
+        if dm_object_class.confidence.value > 100:
+            dm_object_class.confidence.value = 100
+        return dm_object_class, dm_object_class.confidence.value
+
+    def aw_perception_callback(self, aw_msg: DynamicObjectWithFeatureArray):
+        for dynamic_object in aw_msg.feature_objects:
+            dm_object = ObjectInfo()
+            # AW uses uuid (big endian), but dm uses uint64, use only the 64 most significant bits
+            dm_object.id = int.from_bytes(dynamic_object.id.uuid[:8], 'big')
+            object_class, confidence = self.aw_class_to_dm(dynamic_object)
+            dm_object.object_class.append(object_class)
+            dm_object.existency = confidence
 
 
 def main(args=None):
