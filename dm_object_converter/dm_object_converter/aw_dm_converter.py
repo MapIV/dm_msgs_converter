c6da6bc8677fcc8ea76c0724688038a98e4291e1
PE <abraham.monrroy@tier4.jp>
2022-06-15 02:38:35 +0000

    aw_dm converter
    Signed-off-by: PE <abraham.monrroy@tier4.jp>

--- dm_object_converter/dm_object_converter/aw_dm_converter.py
+++ dm_object_converter/dm_object_converter/aw_dm_converter.py
@@ -18,6 +18,7 @@ from dm_object_info_msgs.msg import ThrottlePosition
 from dm_object_info_msgs.msg import ExteriorLights
 from dm_object_info_msgs.msg import VehicleRole
 from dm_object_info_msgs.msg import VehicleExtendedInformation
+from dm_object_info_msgs.msg import ExistenceConfidence
 
 from autoware_perception_msgs.msg import DynamicObjectWithFeatureArray
 from autoware_perception_msgs.msg import DynamicObjectWithFeature
@@ -48,30 +49,32 @@ class AwDmConverter(Node):
             dm_object = ObjectInfo()
             # 物標 ID REQUIRED
             # AW uses uuid (big endian), but dm uses uint64, use only the 64 most significant bits
-            dm_object.id.value = int.from_bytes(aw_dynamic_object.id.uuid[:8], 'big')
+            dm_object.id.value = int.from_bytes(aw_dynamic_object.object.id.uuid[:8], 'big')
             # 情報取得時刻［必須］Timestamp REQUIRED
             # DE_TimestampIts: Number of milliseconds since 2004-01-01T00:00:00.000Z
-            dm_object.time.value = ros_stamp_to_de_time(aw_msg.header.stamp)
+            dm_object.time = ros_stamp_to_de_time(aw_msg.header.stamp)
             # 物標種別 OPTIONAL
-            object_class, confidence = aw_class_to_dm_object_class(aw_dynamic_object)
+            object_class, confidence = aw_class_to_dm_object_class(aw_dynamic_object.object)
             dm_object.object_class.append(object_class)
             # 存在信頼度 OPTIONAL
-            dm_object.existency = confidence
+            existency = ExistenceConfidence()
+            existency.value = confidence
+            dm_object.existency = existency
             # 物標位置
-            dm_object.object_location = aw_position_to_dm_location(dynamic_object=aw_dynamic_object,
+            dm_object.object_location = aw_position_to_dm_location(dynamic_object=aw_dynamic_object.object,
                                                                    altitude=self._altitude,
                                                                    plane_number=self._plane_number)
             # 物標参照位置 dm_object.ref_point.value = UNKNOWN
             # 移動方向 Heading WGS84Angle
-            dm_object.direction = aw_pose_to_dm_direction(aw_dynamic_object)
+            dm_object.direction = aw_pose_to_dm_direction(aw_dynamic_object.object)
             # 速さ Speed
-            dm_object.speed = aw_twist_to_dm_speed(aw_dynamic_object)
+            dm_object.speed = aw_twist_to_dm_speed(aw_dynamic_object.object)
             # 回転速度 YawRate
-            dm_object.yaw_rate = aw_twist_to_dm_yaw_rate(aw_dynamic_object)
+            dm_object.yaw_rate = aw_twist_to_dm_yaw_rate(aw_dynamic_object.object)
             # 前後加速度 Acceleration dm_object.acceleration UNKNOWN
             # 物標の向き WGS84Angle dm_object.orientation UNKNOWN
             # 物標のサイズ
-            dm_object.size = aw_shape_to_dm_size(aw_dynamic_object)
+            dm_object.size = aw_shape_to_dm_size(aw_dynamic_object.object)
             # 物標の色 dm_object.color = ObjectColor.UNKNOWN
             # 前輪舵角 dm_object.steering_angle_front = SteeringAngle.UNKNOWN
             # 後輪舵角 dm_object.steering_angle_rear = SteeringAngle.UNKNOWN
@@ -86,7 +89,7 @@ class AwDmConverter(Node):
             # 情報源のリスト
             data_source = ObjectId()
             data_source.value = VENDOR_ID
-            dm_object.information_source_list.append()
+            dm_object.information_source_list.append(data_source)
 
             # Add complete object to the array
             dm_object_info_array.array.append(dm_object)
--- dm_object_converter/dm_object_converter/dm_conversions.py
+++ dm_object_converter/dm_object_converter/dm_conversions.py
@@ -24,43 +24,62 @@ ALTITUDE_UNAVAILABLE = 0.800001
 
 def aw_shape_to_dm_size(dynamic_object: DynamicObject) -> ObjectSize:
     from dm_object_info_msgs.msg import ObjectDimensionAccuracy
+    from dm_object_info_msgs.msg import ObjectDimensionValue
     size = ObjectSize()  # 0,1 m
-    size.width.value = dynamic_object.shape.dimensions.y * 10
-    size.width.accuracy = int(ObjectDimensionAccuracy.MAX * 0.8)
-    size.height.value = dynamic_object.shape.dimensions.z * 10
-    size.height.accuracy = int(ObjectDimensionAccuracy.MAX * 0.8)
-    size.length.value = dynamic_object.shape.dimensions.x * 10
-    size.length.accuracy = int(ObjectDimensionAccuracy.MAX * 0.8)
+    dm_w = ObjectDimensionValue()
+    dm_accuracy = ObjectDimensionAccuracy()
+    dm_accuracy.value = int(ObjectDimensionAccuracy.MAX * 0.8)
+
+    dm_w.value = int(dynamic_object.shape.dimensions.y * 100)
+    size.width.value = dm_w
+    size.width.accuracy = dm_accuracy
+    
+    dm_h = ObjectDimensionValue()
+    dm_h.value = int(dynamic_object.shape.dimensions.z * 100)
+    size.height.value = dm_h
+    size.height.accuracy = dm_accuracy
+    
+    dm_l = ObjectDimensionValue()
+    dm_l.value = int(dynamic_object.shape.dimensions.x * 100)
+    size.length.value = dm_l
+    size.length.accuracy = dm_accuracy
     return size
 
 
 def aw_twist_to_dm_yaw_rate(dynamic_object: DynamicObject) -> YawRate:
     from dm_object_info_msgs.msg import YawRateAccuracy
+    from dm_object_info_msgs.msg import YawRateValue
     yaw_rate = YawRate()
 
     tmp_yaw = math.degrees(math.sqrt(dynamic_object.state.twist_covariance.twist.angular.x *
                                      dynamic_object.state.twist_covariance.twist.angular.x +
                                      dynamic_object.state.twist_covariance.twist.angular.y *
                                      dynamic_object.state.twist_covariance.twist.angular.y))
-    yaw_rate.value = tmp_yaw * 100  # 0,01 degree per second
+    dm_yaw_rate_value = YawRateValue()
+    dm_yaw_rate_value.value = int(tmp_yaw * 100)
+    yaw_rate.value = dm_yaw_rate_value  # 0,01 degree per second
     yaw_rate.accuracy.value = YawRateAccuracy.MAX
     return yaw_rate
 
 
 def aw_twist_to_dm_speed(dynamic_object: DynamicObject) -> Speed:
     from dm_object_info_msgs.msg import SpeedAccuracy
+    from dm_object_info_msgs.msg import SpeedValue
     speed = Speed()
     tmp_speed = math.sqrt(dynamic_object.state.twist_covariance.twist.linear.x *
                           dynamic_object.state.twist_covariance.twist.linear.x +
                           dynamic_object.state.twist_covariance.twist.linear.y *
                           dynamic_object.state.twist_covariance.twist.linear.y)
-    speed.value = tmp_speed * 100  # 0,01 m/s units
+    dm_speed_value = SpeedValue()
+    dm_speed_value.value = int(tmp_speed * 100)
+    speed.value = dm_speed_value  # 0,01 m/s units
     speed.accuracy.value = SpeedAccuracy.MAX
     return speed
 
 
 def aw_pose_to_dm_direction(dynamic_object: DynamicObject) -> WGS84Angle:
     from dm_object_info_msgs.msg import WGS84AngleAccuracy
+    from dm_object_info_msgs.msg import WGS84AngleValue
     angle = WGS84Angle()
     # angle.value
     roll, pitch, yaw = euler_from_quaternion([dynamic_object.state.pose_covariance.pose.orientation.x,
@@ -81,7 +100,11 @@ def aw_pose_to_dm_direction(dynamic_object: DynamicObject) -> WGS84Angle:
     # ……
     # 36000: 使用しない
     # 36001: 不明
-    angle.value = yaw
+    dm_angle = WGS84AngleValue()
+    tmp = abs(int(math.degrees(yaw) * 100))
+    print(tmp)
+    dm_angle.value = tmp
+    angle.value = dm_angle
     angle.accuracy.value = int(WGS84AngleAccuracy.MAX * 0.8)
 
     return angle
@@ -90,18 +113,30 @@ def aw_pose_to_dm_direction(dynamic_object: DynamicObject) -> WGS84Angle:
 def aw_position_to_dm_location(dynamic_object: DynamicObject,
                                altitude: float = ALTITUDE_UNAVAILABLE,
                                plane_number: int = 7) -> Location:
+    from dm_object_info_msgs.msg import Latitude
+    from dm_object_info_msgs.msg import Longitude
+    from dm_object_info_msgs.msg import Altitude
+
     dm_location = Location()
     dm_location.geodetic_system.value = dm_object_info_msgs.msg.GeodeticSystem.WGS84
     lat, long = xyp_to_lat_lon(x=dynamic_object.state.pose_covariance.pose.position.x,
                                y=dynamic_object.state.pose_covariance.pose.position.y,
                                plane_num=plane_number)
-    dm_location.latitude = lat * 1e6
-    dm_location.longitude = long * 1e6
-    dm_location.altitude = altitude * 1e6
+    dm_latitude = Latitude()
+    dm_longitude = Longitude()
+    dm_altitude = Altitude()
+
+    dm_latitude.value =int(lat * 1e6)
+    dm_longitude.value = int(long * 1e6)
+    dm_altitude.value = int(altitude * 1e6)
+
+    dm_location.latitude = dm_latitude
+    dm_location.longitude = dm_longitude
+    dm_location.altitude = dm_altitude
 
     dm_location.crp_id.value = 0  # Unknown
-    dm_location.dx_crp.value = dynamic_object.state.pose_covariance.pose.position.x * 1e6
-    dm_location.dy_crp.value = dynamic_object.state.pose_covariance.pose.position.y * 1e6
+    dm_location.dx_crp.value = int(dynamic_object.state.pose_covariance.pose.position.x * 1e6)
+    dm_location.dy_crp.value = int(dynamic_object.state.pose_covariance.pose.position.y * 1e6)
 
     # dm_location.lane_count.value =
     # dm_location.lane_position.value =
@@ -135,7 +170,7 @@ def aw_class_to_dm_object_class(dynamic_object: DynamicObject) -> (ObjectClass, 
         dm_object_class.id.value = ClassId.ANIMAL
     if dynamic_object.semantic.type == Semantic.UNKNOWN:
         dm_object_class.id.value = ClassId.OTHER
-    dm_object_class.confidence.value = dynamic_object.semantic.confidence * 100
+    dm_object_class.confidence.value = int(dynamic_object.semantic.confidence * 100)
     if dm_object_class.confidence.value > 100:
         dm_object_class.confidence.value = 100
     return dm_object_class, dm_object_class.confidence.value
@@ -144,6 +179,6 @@ def aw_class_to_dm_object_class(dynamic_object: DynamicObject) -> (ObjectClass, 
 def ros_stamp_to_de_time(ros_stamp: Time) -> TimestampIts:
     dm_time = TimestampIts()
     de_ts_ms = datetime.datetime(2004, 1, 1, 0, 0).timestamp() * 1000  # ISO 8601
-    ros_ts_ms = Time.from_msg(ros_stamp).nanoseconds / 1e6  # epoch time
-    dm_time.value = ros_ts_ms - de_ts_ms
+    ros_ts_ms = Time.from_msg(ros_stamp).nanoseconds / 1e3  # epoch time
+    dm_time.value = int(ros_ts_ms - de_ts_ms)
     return dm_time
